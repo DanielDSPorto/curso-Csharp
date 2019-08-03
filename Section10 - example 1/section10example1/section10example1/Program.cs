c4bb60f07700c6ea234d09be07b9ea8a85e70b3a
Daniel Porto <danielporto@gmail.com>
2019-08-02 22:46:15 -0300

    Section 10 's first example

---
+++ Section10 - example 1/section10example1/section10example1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using section10example1.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace section10example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the number of employees: ");
+            int numberEmployees = int.Parse(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+            for(int i=0; i< numberEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i+1}");
+                Console.Write("Is he an outsourced employee? (y/n): ");
+                string outsourced = Console.ReadLine();
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double vph = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(outsourced == "y")
+                {
+                    Console.Write("Additional charge: ");
+                    double ac = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, vph, ac));
+                } 
+                else
+                {
+                    list.Add(new Employee(name, hours, vph));
+                }
+            }
+            Console.WriteLine("\nPAYMENTS:");
+            foreach (var emp in list)
+                Console.WriteLine(emp.ToString());
+            Console.ReadLine();
+        }
+    }
+}
