eca60c9b97117ea3846cc12e6c65d5b36adc6105
Daniel Dias da Silva Porto <daniel.porto@siemens.com>
2019-08-06 14:25:02 -0300

    Section 10 - Final Exercise

---
+++ Section10 - final exercise/Section10finalExercise/Section10finalExercise/Entities/Individual.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section10finalExercise.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual()
+        {
+
+        }
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            var taxRange = (AnualIncome < 20000.00) ? (0.15) : (0.25);
+            return (AnualIncome * taxRange) - (0.5 * HealthExpenditures);
+        }
+    }
+}
---
+++ Section10 - final exercise/Section10finalExercise/Section10finalExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Section10finalExercise.Entities;
+
+namespace Section10finalExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<TaxPayer>();
+            Console.Write("Enter the number of tax payers: ");
+            var numberOfTaxPayers = int.Parse(Console.ReadLine());
+            for (var i=0; i<numberOfTaxPayers; i++)
+            {
+                Console.WriteLine($"Tax payer #{i+1} data:");
+                Console.Write("Individual or company (i/c)? ");
+                var ch = Console.ReadLine();
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                var aI = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(ch == "i")
+                {
+                    Console.Write("Health Expenditures: ");
+                    var hE = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, aI, hE));
+                }
+                else
+                {
+                    Console.Write("Number of Employees: ");
+                    var nOE = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Company(name, aI, nOE));
+                }
+            }
+            Console.WriteLine("TAXES PAID: ");
+            var accumulatedValue = 0.0;
+
+            foreach (var taxpayer in list)
+            {
+                accumulatedValue += taxpayer.Tax();
+                Console.WriteLine(taxpayer.ToString());
+            }
+            Console.WriteLine($"\nTOTAL TAXES: $ {accumulatedValue}");
+            Console.ReadLine();
+        }
+    }
+}
